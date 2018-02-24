3900b08afc29098d65ba768eff6c730d2d6b168d
unknown <aseelaldallal@gmail.com>
2018-02-24 13:15:11 -0500

    Bug fixes: Enable signup button when nonrequired fields are empty

--- src/components/UI/ImageUpload/ImageUpload.js
+++ src/components/UI/ImageUpload/ImageUpload.js
@@ -4,6 +4,7 @@ import './styles.css';
 const ImageUpload = props => {
   return (
     <div className="imageUpload">
+      <label> {props.label} </label>
       {props.error ? <p className="error">{props.error}</p> : null}
       <img className="image" src={props.imgURL} />
       <input
